23e83d22d4b4fd43bc3e2399d6706da995199878
Miguel Galves <miguel.galves@jwt.com>
2013-10-11 09:50:57 -0300

    Refactoring: performance moved to tests folder

--- redisco/tests/performance.py
+++ redisco/tests/performance.py
@@ -2,31 +2,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
 import redis
-import rom
 import time
 from redisco import models
 
 REDIS = redis.Redis()
-
-
-class Post(rom.Model):
-    uid = rom.String(index=True, unique=True) # ID unico interno
-    postid = rom.String() # Id do provider
-    text = rom.String() # Texto / caption
-    url = rom.String() # URL post original
-    media_url = rom.String(default=None) # Link de eventual midia (video, imagem)
-    provider = rom.Integer(index=True) # Provider (Facebook, Twitter, Vimeo, Youtube, Instagram)
-    post_type = rom.Integer(index=True) # Tipo de post (video, texto, imagem)
-    locale = rom.String()  # Lingua do post
-    brand = rom.Integer(index=True) # Marca
-    userid = rom.String(index=True) # Userid
-    user_name = rom.String() # User name
-    user_avatar = rom.String() # User Avatar
-    country = rom.Integer(index=True, required=False) # Pais de origem
-    is_own_post = rom.Boolean(index=True) # Post gerado pelo dono da marca?
-    interactions = rom.Integer(index=True, default=0) # Numero de interacoes do post
-    ts = rom.Integer(index=True) # Timestamp de criacao do post
-
 
 class RediscoPost(models.Model):
     uid = models.Attribute(indexed=False) # ID unico interno
@@ -46,52 +25,7 @@ class RediscoPost(models.Model):
     interactions = models.IntegerField() # Numero de interacoes do post
     ts = models.IntegerField(indexed=False) # Timestamp de criacao do post
 
-    class Meta:
-        pickle = True
-
 from datetime import datetime
-# REDIS.flushall()
-
-# start = datetime.now()
-# for i in range(0, 10000):
-#     post = Post(uid = i,
-#             postid = i,
-#             text = "text",
-#             url = "http",
-#             provider = 2,
-#             post_type = 1,
-#             media_url = "",
-#             locale = "pt",
-#             brand = 1,
-#             country = None,
-#             userid = i,
-#             user_name = "bla",
-#             user_avatar = "",
-#             is_own_post = True,
-#             interactions = 10,
-#             ts = i
-#         )
-#     post.save()
-# delta = datetime.now()-start
-# print "SAVE ROM", delta
-
-# rom.session.commit(all=True, full=True)
-
-# start = datetime.now()
-# for i in range(0, 10000):
-#     Post.get(i)
-# delta = datetime.now()-start
-# print "GET ROM", delta
-
-# rom.session.commit(all=True, full=True)
-
-# start = datetime.now()
-# Post.query.filter(provider=2).execute()
-# delta = datetime.now()-start
-# print "QUERY ROM", delta
-
-# print
-# print
 
 # REDIS.flushall()
 # import cPickle
@@ -169,6 +103,12 @@ delta = datetime.now()-start
 print "GET REDISCO", delta
 
 start = datetime.now()
+for i in range(0, 10000):
+    RediscoPost.objects.values().get_by_id(i)
+delta = datetime.now()-start
+print "GET REDISCO VALUES", delta
+
+start = datetime.now()
 posts = RediscoPost.objects.filter(provider=2)
 for p in posts:
     p.uid
@@ -176,6 +116,19 @@ delta = datetime.now()-start
 print "QUERY REDISCO", delta, len(posts)
 
 start = datetime.now()
+posts = RediscoPost.objects.values().filter(provider=2)
+for p in posts:
+    p["uid"]
+delta = datetime.now()-start
+print "QUERY REDISCO VALUES", delta, len(posts)
+
+start = datetime.now()
 posts = RediscoPost.objects.filter(provider=2)[:]
 delta = datetime.now()-start
 print "QUERY REDISCO SLICE", delta, len(posts)
+
+
+start = datetime.now()
+posts = RediscoPost.objects.values().filter(provider=2)[:]
+delta = datetime.now()-start
+print "QUERY REDISCO SLICE VALUES", delta, len(posts)
