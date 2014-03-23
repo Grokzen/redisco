f68094b65a4f2d34082b50691424443514eb349f
Grokzen <Grokzen@gmail.com>
2014-03-23 21:46:10 +0100

    Removed commented code

--- redisco/models/basetests.py
+++ redisco/models/basetests.py
@@ -164,7 +164,6 @@ class ModelTestCase(RediscoTestCase):
         person = Person.objects.create(first_name="Granny", last_name="Goose")
         db = person.db
         key = person.key(att='_indices')
-        # ckey = Person._key
 
         index = 'Person:first_name:%s' % "Granny"
         self.assertTrue(index in db.smembers(key))
@@ -184,8 +183,6 @@ class ModelTestCase(RediscoTestCase):
         class Event(models.Model):
             name = models.CharField(required=True)
             created_on = models.DateField(required=True)
-
-        # from datetime import date
 
         Event.objects.create(name="Event #1", created_on=date.today())
         Event.objects.create(name="Event #2", created_on=date.today())
@@ -1054,8 +1051,6 @@ class MutexTestCase(RediscoTestCase):
 
     def test_lock_expired(self):
         Mutex(self.p1).lock()
-        # with Mutex(self.p2):
-        #     self.assert_(True)
         with self.assertRaises(RuntimeError):
             with Mutex(self.p2, timeout=1):
                 self.assertTrue(1)
