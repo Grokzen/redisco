4148d6374b642ea147f2b0fc8c0b7dfd15ba0d47
Johan Andersson <Grokzen@gmail.com>
2014-04-24 18:20:54 +0200

    Removed the workaround because it was allready implemented correctly in redis-py if you add 'decode_response': True as input argument to Redis client object.

--- redisco/__init__.py
+++ redisco/__init__.py
@@ -2,40 +2,12 @@
 
 import redis
 
-# Fix a bug in redis-py when using it for python3 and withouth hiredis
-from redis import connection
-connection.DefaultParser.encoding = "UTF-8"
-
-# Fix a bug in redis-py when using hiredis with python3
-if connection.DefaultParser.__name__ == "HiredisParser":
-    orig_read_response = connection.DefaultParser.read_response
-
-    def read_response_patched(self):
-        response = orig_read_response(self)
-
-        if isinstance(response, bytes) and self.encoding:
-            response = response.decode(self.encoding)
-        if isinstance(response, list) and self.encoding:
-            out = []
-            for item in response:
-                if isinstance(item, list):
-                    # We got nested list, decode sub-list
-                    out.append([i.decode(self.encoding) if isinstance(i, bytes) else i for i in item])
-                elif isinstance(item, bytes):
-                    out.append(item.decode(self.encoding))
-                else:
-                    out.append(item)
-            response = out
-        return response
-
-    # Monkey patch the original function with the patched one
-    connection.DefaultParser.read_response = read_response_patched
-
 
 class Client(object):
     def __init__(self, **kwargs):
         self.connection_settings = kwargs or {'host': 'localhost',
-                                              'port': 6379, 'db': 0}
+                                              'port': 6379, 'db': 0,
+                                              'decode_responses': True}
 
     def redis(self):
         return redis.Redis(**self.connection_settings)
